79cd3ee258a046c2b00c72052c9ca094292526eb
Misbha147 <misbha645@gmail.com>
2025-05-27 15:56:22 +0530

    save

---
+++ BankCustomer.java
@@ -0,0 +1,23 @@
+package Program1;
+public class BankCustomer {
+    private String custName;
+    private int customerId;
+    private String customerAddress;
+
+    public BankCustomer(String custName, int customerId, String customerAddress) {
+        this.custName = custName;
+        this.customerId = customerId;
+        this.customerAddress = customerAddress;
+    }
+
+    public void displayCustomerDetails() {
+        System.out.println("Customer Name is: " + custName);
+        System.out.println("Customer ID is: " + customerId);
+        System.out.println("Customer Address is: " + customerAddress);
+    }
+
+    public static void main(String[] args) {
+        BankCustomer c = new BankCustomer("Misbha", 1, "Hubli-20");
+        c.displayCustomerDetails();
+    }
+}
